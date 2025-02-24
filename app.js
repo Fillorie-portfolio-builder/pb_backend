a82268c9df68f0086100f8f7744d907f1a9c2261
madura <madura@onestep.lk>
2025-02-24 16:05:43 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// src/app.js
+const express = require('express');
+const cors = require('cors');
+// const auth = require('./src/routes/auth');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+// app.use('/api/auth', auth);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
